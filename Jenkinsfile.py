b4ab82c116103228ea705fd441fa2a06c6d3e04d
Poorvika N P <poorvikanp245@gmail.com>
2026-03-13 09:21:48 +0530

    Update Jenkinsfile.py

--- Jenkinsfile.py
+++ Jenkinsfile.py
@@ -1,10 +1,17 @@
 pipeline {
-  agent any
-  stages {
-      stage('Compile java Program'){
-         steps {
-             bat 'javac HelloWorld.java'
-         }
-}
-}
+    agent any
+    stages {
+        stage('Compile java Program') {
+            steps {
+                
+                bat 'javac HelloWorld.java'
+            }
+        }
+        stage('Run java Program') {
+            steps {
+              
+                bat 'java HelloWorld'
+            }
+        }
+    }
 }
